3a686cd6e89f4dd812db314b733796aabf453cb0
Vaughn Vernon <vaughn@forcomprehension.com>
2018-05-19 15:24:45 -0700

    Fixed angle brackets for Javadoc.

--- src/main/java/io/vlingo/cluster/model/message/OperationalMessage.java
+++ src/main/java/io/vlingo/cluster/model/message/OperationalMessage.java
@@ -13,57 +13,57 @@ import io.vlingo.wire.node.Id;
 public abstract class OperationalMessage implements Message {
 
   /**
-   * APP<lf>id=x nm=name si=y<lf>...
+   * APP&lt;lf&gt;id=x nm=name si=y&lt;lf&gt;...
    */
   public static final String APP = "APP";
 
   /**
-   * CHECKHEALTH<lf>id=x ***internal only*** to check health
+   * CHECKHEALTH&lt;lf&gt;id=x ***internal only*** to check health
    */
   public static final String CHECKHEALTH = "CHECKHEALTH";
 
   /**
-   * DIR<lf>id=x nm=name<lf>addr=...<lf>...
+   * DIR&lt;lf&gt;id=x nm=name&lt;lf&gt;addr=...&lt;lf&gt;...
    */
   public static final String DIR = "DIR";
 
   /**
-   * ELECT<lf>id=x an election is required
+   * ELECT&lt;lf&gt;id=x an election is required
    */
   public static final String ELECT = "ELECT";
 
   /**
-   * JOIN<lf>addr=... node joining cluster
+   * JOIN&lt;lf&gt;addr=... node joining cluster
    */
   public static final String JOIN = "JOIN";
 
   /**
-   * LEADER<lf>id=x declare self as leader (highest existing node)
+   * LEADER&lt;lf&gt;id=x declare self as leader (highest existing node)
    */
   public static final String LEADER = "LEADER";
 
   /**
-   * LEAVE<lf>id=x announce that node is leaving the cluster
+   * LEAVE&lt;lf&gt;id=x announce that node is leaving the cluster
    */
   public static final String LEAVE = "LEAVE";
 
   /**
-   * PING<lf>id=x leader sends ping to follower to determine if it is alive
+   * PING&lt;lf&gt;id=x leader sends ping to follower to determine if it is alive
    */
   public static final String PING = "PING";
 
   /**
-   * PULSE<lf>id=x follower sends pulse to leader
+   * PULSE&lt;lf&gt;id=x follower sends pulse to leader
    */
   public static final String PULSE = "PULSE";
 
   /**
-   * SPLIT<lf>id=x inform receiving node that it is split from known leader
+   * SPLIT&lt;lf&gt;id=x inform receiving node that it is split from known leader
    */
   public static final String SPLIT = "SPLIT";
 
   /**
-   * VOTE<lf>id=x a vote is made by all higher nodes
+   * VOTE&lt;lf&gt;id=x a vote is made by all higher nodes
    */
   public static final String VOTE = "VOTE";
 
