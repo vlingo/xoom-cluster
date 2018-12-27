761fd1693bb644f60af1c4e61f765b8805d6424b
Vaughn Vernon <vaughn@forcomprehension.com>
2018-12-26 23:09:07 -0700

    Use atomic values.

--- src/test/java/io/vlingo/cluster/model/ClusterApplicationBroadcasterTest.java
+++ src/test/java/io/vlingo/cluster/model/ClusterApplicationBroadcasterTest.java
@@ -20,85 +20,85 @@ public class ClusterApplicationBroadcasterTest extends AbstractClusterTest {
   @Test
   public void testInformAllLiveNodes() throws Exception {
     broadcaster.informAllLiveNodes(config.allNodes(), true);
-    assertEquals(1, application.allLiveNodes);
+    assertEquals(1, application.allLiveNodes.get());
   }
 
   @Test
   public void testInformLeaderElected() throws Exception {
     broadcaster.informLeaderElected(Id.of(3), true, false);
-    assertEquals(1, application.informLeaderElected);
+    assertEquals(1, application.informLeaderElected.get());
   }
 
   @Test
   public void testInformLeaderLost() throws Exception {
     broadcaster.informLeaderLost(Id.of(3), false);
-    assertEquals(1, application.informLeaderLost);
+    assertEquals(1, application.informLeaderLost.get());
   }
 
   @Test
   public void testInformLocalNodeShutDown() throws Exception {
     broadcaster.informLocalNodeShutDown(Id.of(1));
-    assertEquals(1, application.informLocalNodeShutDown);
+    assertEquals(1, application.informLocalNodeShutDown.get());
   }
 
   @Test
   public void testInformLocalNodeStarted() throws Exception {
     broadcaster.informLocalNodeStarted(Id.of(1));
-    assertEquals(1, application.informLocalNodeStarted);
+    assertEquals(1, application.informLocalNodeStarted.get());
   }
 
   @Test
   public void testInformNodeIsHealthy() throws Exception {
     broadcaster.informNodeIsHealthy(Id.of(1), true);
-    assertEquals(1, application.informNodeIsHealthy);
+    assertEquals(1, application.informNodeIsHealthy.get());
   }
 
   @Test
   public void testInformNodeJoinedCluster() throws Exception {
     broadcaster.informNodeJoinedCluster(Id.of(2), true);
-    assertEquals(1, application.informNodeJoinedCluster);
+    assertEquals(1, application.informNodeJoinedCluster.get());
   }
 
   @Test
   public void testInformNodeLeftCluster() throws Exception {
     broadcaster.informNodeLeftCluster(Id.of(2), true);
-    assertEquals(1, application.informNodeLeftCluster);
+    assertEquals(1, application.informNodeLeftCluster.get());
   }
 
   @Test
   public void testInformQuorumAchieved() throws Exception {
     broadcaster.informQuorumAchieved();
-    assertEquals(1, application.informQuorumAchieved);
+    assertEquals(1, application.informQuorumAchieved.get());
   }
 
   @Test
   public void testInformQuorumLost() throws Exception {
     broadcaster.informQuorumLost();
-    assertEquals(1, application.informQuorumLost);
+    assertEquals(1, application.informQuorumLost.get());
   }
   
   @Test
   public void testInformAttributeSetCreated() throws Exception {
     broadcaster.informAttributeSetCreated("test");
-    assertEquals(1, application.informAttributeSetCreated);
+    assertEquals(1, application.informAttributeSetCreated.get());
   }
   
   @Test
   public void testInformAttributeAdded() throws Exception {
     broadcaster.informAttributeAdded("test", "test");
-    assertEquals(1, application.informAttributeAdded);
+    assertEquals(1, application.informAttributeAdded.get());
   }
   
   @Test
   public void testInformAttributeRemoved() throws Exception {
     broadcaster.informAttributeRemoved("test", "test");
-    assertEquals(1, application.informAttributeRemoved);
+    assertEquals(1, application.informAttributeRemoved.get());
   }
   
   @Test
   public void testInformAttributeReplaced() throws Exception {
     broadcaster.informAttributeReplaced("test", "test");
-    assertEquals(1, application.informAttributeReplaced);
+    assertEquals(1, application.informAttributeReplaced.get());
   }
   
   @Before
