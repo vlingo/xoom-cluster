e1143db4a4983dd30acce439dea408ce34916960
Vaughn Vernon <vaughn@forcomprehension.com>
2018-12-26 23:05:13 -0700

    Accept optional World parameter.

--- src/main/java/io/vlingo/cluster/model/Cluster.java
+++ src/main/java/io/vlingo/cluster/model/Cluster.java
@@ -7,33 +7,53 @@
 
 package io.vlingo.cluster.model;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import io.vlingo.actors.Logger;
 import io.vlingo.actors.World;
 import io.vlingo.common.Tuple2;
 
 public class Cluster {
   
-  private static final World world = World.start("vlingo-cluster");
-  
-  private static ClusterSnapshotControl control = null;
-  
+  private static AtomicReference<ClusterSnapshotControl> control = new AtomicReference<>();
+  private static World world;
+
   public static final synchronized Tuple2<ClusterSnapshotControl, Logger> controlFor(final String name) throws Exception {
-    if (control != null) {
+    if (world != null) {
       throw new IllegalArgumentException("Cluster snapshot control already exists.");
     }
-    
+    return controlFor(World.start("vlingo-cluster"), name);
+  }
+
+  public static final synchronized Tuple2<ClusterSnapshotControl, Logger> controlFor(final World world, final String name) throws Exception {
+    if (control.get() != null) {
+      throw new IllegalArgumentException("Cluster snapshot control already exists.");
+    }
+
+    Cluster.world = world;
+
     final Tuple2<ClusterSnapshotControl, Logger> control = ClusterSnapshotControl.instance(world, name);
     
-    Cluster.control = control._1;
+    Cluster.control.set(control._1);
     
     return control;
   }
 
   public static boolean isRunning() {
-    return control != null;
+    return control.get() != null;
+  }
+
+  public static boolean isRunning(final boolean expected, final int retries) {
+    for (int idx = 0; idx < retries; ++idx) {
+      if (isRunning() == expected) {
+        return expected;
+      }
+      try { Thread.sleep(500); } catch (Exception e) { }
+    }
+    return !expected;
   }
 
   static final synchronized void reset() {
-    control = null;
+    control.set(null);
   }
 }
