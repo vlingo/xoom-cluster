4c703c06b9b7964c8a3d4e9ecb4942333f7e3598
VaughnVernon <vaughn@forcomprehension.com>
2019-12-09 20:26:25 -0700

    Added missing property. Start World with defaults. Use incrementAndGet().

--- src/test/java/io/vlingo/cluster/model/AbstractClusterTest.java
+++ src/test/java/io/vlingo/cluster/model/AbstractClusterTest.java
@@ -30,7 +30,7 @@ public abstract class AbstractClusterTest extends AbstractMessageTool {
   protected ClusterConfiguration config;
   protected Properties properties;
   protected TestWorld testWorld;
-  
+
   @Test
   public void testValues() throws Exception {
     assertNotNull(application);
@@ -38,56 +38,57 @@ public abstract class AbstractClusterTest extends AbstractMessageTool {
     assertNotNull(properties);
     assertNotNull(testWorld);
   }
-  
+
   @Before
   public void setUp() throws Exception {
     java.util.Properties properties = new java.util.Properties();
-    
+
     properties.setProperty("cluster.ssl", "false");
-    
+
     properties.setProperty("cluster.op.buffer.size", "4096");
     properties.setProperty("cluster.app.buffer.size", "10240");
     properties.setProperty("cluster.op.outgoing.pooled.buffers", "20");
     properties.setProperty("cluster.app.outgoing.pooled.buffers", "50");
-    
+
     properties.setProperty("cluster.msg.charset", "UTF-8");
-    
+
     properties.setProperty("cluster.app.class", "io.vlingo.cluster.model.application.FakeClusterApplicationActor");
-    
+    properties.setProperty("cluster.app.stage", "fake.app.stage");
+
     properties.setProperty("cluster.health.check.interval", "2000");
     properties.setProperty("cluster.live.node.timeout", "20000");
     properties.setProperty("cluster.heartbeat.interval", "7000");
     properties.setProperty("cluster.quorum.timeout", "60000");
-    
+
     properties.setProperty("cluster.seedNodes", "node1,node2,node3");
-    
+
     properties.setProperty("node.node1.id", "1");
     properties.setProperty("node.node1.name", "node1");
     properties.setProperty("node.node1.host", "localhost");
     properties.setProperty("node.node1.op.port", nextPortToUseString());
     properties.setProperty("node.node1.app.port", nextPortToUseString());
-    
+
     properties.setProperty("node.node2.id", "2");
     properties.setProperty("node.node2.name", "node2");
     properties.setProperty("node.node2.host", "localhost");
     properties.setProperty("node.node2.op.port", nextPortToUseString());
     properties.setProperty("node.node2.app.port", nextPortToUseString());
-    
+
     properties.setProperty("node.node3.id", "3");
     properties.setProperty("node.node3.name", "node3");
     properties.setProperty("node.node3.host", "localhost");
     properties.setProperty("node.node3.op.port", nextPortToUseString());
     properties.setProperty("node.node3.app.port", nextPortToUseString());
-    
+
     this.properties = Properties.openForTest(properties);
-    
-    this.testWorld = TestWorld.start("cluster-test-world");
-    
+
+    this.testWorld = TestWorld.startWithDefaults("cluster-test-world");
+
     this.config = new ClusterConfiguration(this.properties, testWorld.defaultLogger());
-    
+
     this.application = new MockClusterApplication();
   }
-  
+
   @After
   public void tearDown() {
     testWorld.terminate();
@@ -96,7 +97,7 @@ public abstract class AbstractClusterTest extends AbstractMessageTool {
 
 
   private int nextPortToUse() {
-    return PORT_TO_USE.getAndIncrement();
+    return PORT_TO_USE.incrementAndGet();
   }
 
   private String nextPortToUseString() {
