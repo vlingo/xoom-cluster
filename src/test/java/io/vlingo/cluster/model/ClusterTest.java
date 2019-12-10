284b2f006ad9aa4286688e25be434b90c37a8069
VaughnVernon <vaughn@forcomprehension.com>
2019-12-09 20:23:11 -0700

    Extended Cluster.controlFor() parameters.

--- src/test/java/io/vlingo/cluster/model/ClusterTest.java
+++ src/test/java/io/vlingo/cluster/model/ClusterTest.java
@@ -7,20 +7,23 @@
 
 package io.vlingo.cluster.model;
 
-import io.vlingo.actors.Logger;
-import io.vlingo.common.Tuple2;
-import org.junit.Test;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.vlingo.actors.Logger;
+import io.vlingo.actors.World;
+import io.vlingo.cluster.model.application.ClusterApplication.DefaultClusterApplicationInstantiator;
+import io.vlingo.common.Tuple2;
 
 public class ClusterTest extends AbstractClusterTest {
   private static int count = 0;
 
   @Test
   public void testClusterSnapshotControl() throws Exception {
-    final Tuple2<ClusterSnapshotControl, Logger> control = Cluster.controlFor("node1");
+    final Tuple2<ClusterSnapshotControl, Logger> control = Cluster.controlFor(World.startWithDefaults("test"), new DefaultClusterApplicationInstantiator(), properties, "node1");
 
     assertNotNull(control);
 
