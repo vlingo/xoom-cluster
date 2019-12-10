633c98d889364aa4853d5ea9d0fb2e339c600958
VaughnVernon <vaughn@forcomprehension.com>
2019-12-09 20:29:49 -0700

    Use ActorInstantiator.

--- src/main/java/io/vlingo/cluster/model/node/LocalLiveNode.java
+++ src/main/java/io/vlingo/cluster/model/node/LocalLiveNode.java
@@ -7,6 +7,7 @@
 
 package io.vlingo.cluster.model.node;
 
+import io.vlingo.actors.ActorInstantiator;
 import io.vlingo.actors.Definition;
 import io.vlingo.actors.Stage;
 import io.vlingo.actors.Stoppable;
@@ -25,18 +26,49 @@ public interface LocalLiveNode extends Stoppable {
           final Registry registry,
           final OperationalOutboundStream outbound,
           final Configuration configuration) {
-    
+
     final Definition definition =
             new Definition(
                     LocalLiveNodeActor.class,
-                    Definition.parameters(node, snapshot, registry, outbound, configuration),
+                    new LocalLiveNodeInstantiator(node, snapshot, registry, outbound, configuration),
                     "local-live-node");
-    
+
     LocalLiveNode localLiveNode = stage.actorFor(LocalLiveNode.class, definition);
-    
+
     return localLiveNode;
   }
-  
+
+  static class LocalLiveNodeInstantiator implements ActorInstantiator<LocalLiveNodeActor> {
+    private final Node node;
+    private final ClusterSnapshot snapshot;
+    private final Registry registry;
+    private final OperationalOutboundStream outbound;
+    private final Configuration configuration;
+
+    public LocalLiveNodeInstantiator(
+            final Node node,
+            final ClusterSnapshot snapshot,
+            final Registry registry,
+            final OperationalOutboundStream outbound,
+            final Configuration configuration) {
+      this.node = node;
+      this.snapshot = snapshot;
+      this.registry = registry;
+      this.outbound = outbound;
+      this.configuration = configuration;
+    }
+
+    @Override
+    public LocalLiveNodeActor instantiate() {
+      return new LocalLiveNodeActor(node, snapshot, registry, outbound, configuration);
+    }
+
+    @Override
+    public Class<LocalLiveNodeActor> type() {
+      return LocalLiveNodeActor.class;
+    }
+  }
+
   void handle(final OperationalMessage message);
   void registerNodeSynchronizer(final NodeSynchronizer nodeSynchronizer);
 }
