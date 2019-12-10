02e2c3639e54ef736e8bb4fa41c26574a8295247
VaughnVernon <vaughn@forcomprehension.com>
2019-12-09 20:24:20 -0700

    Debug output (disabled).

--- src/test/java/io/vlingo/cluster/model/ClusterSnapshotActorTest.java
+++ src/test/java/io/vlingo/cluster/model/ClusterSnapshotActorTest.java
@@ -34,34 +34,44 @@ import io.vlingo.wire.node.Node;
 public class ClusterSnapshotActorTest extends AbstractClusterTest {
   private RawMessage opMessage;
   private ClusterSnapshotInitializer intializer;
-  
+
   @Test
   public void testClusterSnapshot() throws Exception {
+//    System.out.println("=========== testClusterSnapshot ===========");
+//    System.out.println(" OP: " + intializer.localNode().operationalAddress());
+//    System.out.println("APP: " + intializer.localNode().applicationAddress());
+
     final TestActor<ClusterSnapshot> snapshot =
             testWorld.actorFor(
                     ClusterSnapshot.class,
                     Definition.has(ClusterSnapshotActor.class, Definition.parameters(intializer, application)));
-    
+
     snapshot.actor().quorumAchieved();
     assertEquals(1, application.informQuorumAchieved.get());
-    
+
     snapshot.actor().quorumLost();
     assertEquals(1, application.informQuorumLost.get());
   }
 
   @Test
   public void testClusterSnapshotControl() throws Exception {
+//    System.out.println("=========== testClusterSnapshotControl ===========");
+//    System.out.println(" OP: " + intializer.localNode().operationalAddress());
+//    System.out.println("APP: " + intializer.localNode().applicationAddress());
     final TestActor<ClusterSnapshotControl> control =
             testWorld.actorFor(
                     ClusterSnapshotControl.class,
                     Definition.has(ClusterSnapshotActor.class, Definition.parameters(intializer, application)));
-    
+
     control.actor().shutDown();
     assertEquals(1, application.stop.get());
   }
 
   @Test
   public void testInboundStreamInterest() throws Exception {
+//    System.out.println("=========== testInboundStreamInterest ===========");
+//    System.out.println(" OP: " + intializer.localNode().operationalAddress());
+//    System.out.println("APP: " + intializer.localNode().applicationAddress());
     final TestActor<InboundStreamInterest> inboundStreamInterest =
             testWorld.actorFor(
                     InboundStreamInterest.class,
@@ -69,7 +79,7 @@ public class ClusterSnapshotActorTest extends AbstractClusterTest {
 
     inboundStreamInterest.actor().handleInboundStreamMessage(AddressType.OP, opMessage);
     assertEquals(0, application.handleApplicationMessage.get());
-    
+
     final RawMessage appMessage = RawMessage.from(1, 0, "app-test");
     inboundStreamInterest.actor().handleInboundStreamMessage(AddressType.APP, appMessage);
     assertEquals(1, application.handleApplicationMessage.get());
@@ -77,20 +87,23 @@ public class ClusterSnapshotActorTest extends AbstractClusterTest {
 
   @Test
   public void testRegistryInterest() throws Exception {
+//    System.out.println("=========== testRegistryInterest ===========");
+//    System.out.println(" OP: " + intializer.localNode().operationalAddress());
+//    System.out.println("APP: " + intializer.localNode().applicationAddress());
     final TestActor<RegistryInterest> registryInterest =
             testWorld.actorFor(
                     RegistryInterest.class,
                     Definition.has(ClusterSnapshotActor.class, Definition.parameters(intializer, application)));
-    
+
     registryInterest.actor().informAllLiveNodes(config.allNodes(), true);
     assertEquals(1, application.allLiveNodes.get());
-    
+
     registryInterest.actor().informConfirmedByLeader(config.nodeMatching(Id.of(1)), true);
     assertEquals(1, application.informNodeIsHealthy.get());
-    
+
     registryInterest.actor().informCurrentLeader(config.nodeMatching(Id.of(3)), true);
     assertEquals(1, application.informLeaderElected.get());
-    
+
     List<Node> nodes = new ArrayList<>();
     nodes.add(config.nodeMatching(Id.of(3)));
     List<MergeResult> mergeResult = new ArrayList<>();
@@ -99,35 +112,40 @@ public class ClusterSnapshotActorTest extends AbstractClusterTest {
     registryInterest.actor().informMergedAllDirectoryEntries(nodes, mergeResult, true);
     assertEquals(1, application.informNodeJoinedCluster.get());
     assertEquals(1, application.informNodeLeftCluster.get());
-    
+
     registryInterest.actor().informLeaderDemoted(config.nodeMatching(Id.of(2)), true);
     assertEquals(1, application.informLeaderLost.get());
-    
+
     registryInterest.actor().informNodeIsHealthy(config.nodeMatching(Id.of(2)), true);
     assertEquals(2, application.informNodeIsHealthy.get());
-    
+
     registryInterest.actor().informNodeJoinedCluster(config.nodeMatching(Id.of(2)), true);
     assertEquals(2, application.informNodeJoinedCluster.get());
-    
+
     registryInterest.actor().informNodeLeftCluster(config.nodeMatching(Id.of(2)), true);
     assertEquals(2, application.informNodeLeftCluster.get());
-    
+
     registryInterest.actor().informNodeTimedOut(config.nodeMatching(Id.of(2)), true);
     assertEquals(3, application.informNodeLeftCluster.get());
   }
-  
+
+  @Override
   @Before
   public void setUp() throws Exception {
     super.setUp();
-    
+
     intializer = new ClusterSnapshotInitializer("node1", properties, testWorld.defaultLogger());
-    
+//    System.out.println("=========== setUp ===========");
+//    System.out.println(" OP: " + properties.operationalPort("node1"));
+//    System.out.println("APP: " + properties.applicationPort("node1"));
+
     final ByteBuffer messageBuffer = ByteBufferAllocator.allocate(4096);
     final Pulse pulse = new Pulse(Id.of(1));
     MessageConverters.messageToBytes(pulse, messageBuffer);
     opMessage = Converters.toRawMessage(Id.of(1).value(), messageBuffer);
   }
-  
+
+  @Override
   @After
   public void tearDown() {
     super.tearDown();
