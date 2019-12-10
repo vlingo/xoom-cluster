fa168d65a4feb9237d50cf488267fa81d77e8c2b
VaughnVernon <vaughn@forcomprehension.com>
2019-12-09 20:34:22 -0700

    Require Properties parameter. Use ActorInstantiator.

--- src/main/java/io/vlingo/cluster/model/ClusterSnapshotControl.java
+++ src/main/java/io/vlingo/cluster/model/ClusterSnapshotControl.java
@@ -7,28 +7,63 @@
 
 package io.vlingo.cluster.model;
 
+import io.vlingo.actors.ActorInstantiator;
 import io.vlingo.actors.Definition;
 import io.vlingo.actors.Logger;
+import io.vlingo.actors.Stage;
 import io.vlingo.actors.World;
 import io.vlingo.cluster.model.application.ClusterApplication;
+import io.vlingo.cluster.model.application.ClusterApplication.ClusterApplicationInstantiator;
 import io.vlingo.common.Tuple2;
 
 public interface ClusterSnapshotControl {
-  public static Tuple2<ClusterSnapshotControl, Logger> instance(final World world, final String name) {
-    final ClusterSnapshotInitializer initializer = new ClusterSnapshotInitializer(name, Properties.instance, world.defaultLogger());
-    
-    final ClusterApplication application = ClusterApplication.instance(world, initializer.localNode());
-    
+  public static Tuple2<ClusterSnapshotControl, Logger> instance(
+          final World world,
+          final ClusterApplicationInstantiator<?> instantiator,
+          final Properties properties,
+          final String name) {
+
+    final ClusterSnapshotInitializer initializer = new ClusterSnapshotInitializer(name, properties, world.defaultLogger());
+    instantiator.node(initializer.localNode());
+
+    final String stageName = properties.clusterApplicationStageName();
+    final Stage stage = world.stageNamed(stageName);
+    final ClusterApplication application = ClusterApplication.instance(stage, instantiator);
+
     final Definition definition =
             new Definition(
                     ClusterSnapshotActor.class,
-                    Definition.parameters(initializer, application),
+                    new ClusterSnapshotInstantiator(initializer, application),
                     "cluster-snapshot-" + name);
-    
+
     ClusterSnapshotControl control = world.actorFor(ClusterSnapshotControl.class, definition);
-    
+
     return Tuple2.from(control, world.defaultLogger());
   }
 
   void shutDown();
+
+  static class ClusterSnapshotInstantiator implements ActorInstantiator<ClusterSnapshotActor> {
+    final ClusterApplication application;
+    final ClusterSnapshotInitializer initializer;
+
+    public ClusterSnapshotInstantiator(final ClusterSnapshotInitializer initializer, final ClusterApplication application) {
+      this.initializer = initializer;
+      this.application = application;
+    }
+
+    @Override
+    public ClusterSnapshotActor instantiate() {
+      try {
+        return new ClusterSnapshotActor(initializer, application);
+      } catch (Exception e) {
+        throw new IllegalArgumentException("Failed to instantiate " + type() + " because: " + e.getMessage(), e);
+      }
+    }
+
+    @Override
+    public Class<ClusterSnapshotActor> type() {
+      return ClusterSnapshotActor.class;
+    }
+  }
 }
