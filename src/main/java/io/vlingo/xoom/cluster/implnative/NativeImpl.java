f0b2515c5418e46d8a19e0b41e2ef08394d00117
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-11 07:58:50 +0100

    Adding GraalVM Dashboard Support..

---
+++ src/main/java/io/vlingo/xoom/cluster/implnative/NativeImpl.java
@@ -0,0 +1,18 @@
+package io.vlingo.xoom.cluster.implnative;
+
+import io.vlingo.xoom.actors.World;
+import io.vlingo.xoom.cluster.model.ClusterConfiguration;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+
+public final class NativeImpl {
+  @CEntryPoint(name = "Java_io_vlingo_xoom_clusternative_Native_start")
+  public static int start(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer name) {
+    final String nameString = CTypeConversion.toJavaString(name);
+    World world = World.start(nameString);
+    new ClusterConfiguration(null, world.defaultLogger());
+
+    return 0;
+  }
+}
