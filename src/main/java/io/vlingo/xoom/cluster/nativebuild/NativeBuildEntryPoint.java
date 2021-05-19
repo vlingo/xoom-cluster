2fc33cbaf366f887b951b0f68404b792a3281af4
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-19 21:05:34 +0100

    Refactoring..

--- src/main/java/io/vlingo/xoom/cluster/nativebuild/NativeBuildEntryPoint.java
+++ src/main/java/io/vlingo/xoom/cluster/nativebuild/NativeBuildEntryPoint.java
@@ -1,4 +1,4 @@
-package io.vlingo.xoom.cluster.implnative;
+package io.vlingo.xoom.cluster.nativebuild;
 
 import io.vlingo.xoom.actors.World;
 import io.vlingo.xoom.cluster.model.ClusterConfiguration;
